83176277b8585529067487dd9f622cef614c45e0
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-11 19:27:24 -0500

    Commit 2

---
+++ notes.js
@@ -0,0 +1,6 @@
+// GET, PUT, POST, PATCH, DELETE from each API (dog-ceo) (jsonplaceholder)
+// https://www.guru99.com/postman-tutorial.html
+
+// Please read and follow along with the tutorial as a group. Engage your group in discussion through every topic and ensure every member of your group understands before moving to the next topic. Submit a screenshot of your own Postman to titus after finishing the last topic.
+
+// You may omit the last section: How to Run Collections using Newman.
